811c95b3d073fb70099f1c0043e44fd34dbb3ec3
Binkledurg <mhendon423@live.com>
2020-09-15 20:51:17 +0000

    your commit message

---
+++ challenges/car_dict.py
@@ -0,0 +1,23 @@
+# List of cars dictionary
+cars = [{"type": "audi", "year": "1999", "color": "Purple"}, {"type": "bmw", "year": "2015", "color": "Black"}, {"type": "Tesla", "year": "2020", "color": "Black"}]
+
+# "I want a <color> <type> from <year>
+
+
+#car1 = f"I want a {cars[0]['color']} from {cars[0]['year']}"
+
+for car in cars:
+    print(f"I want a {car['color']} {car['type']} from {car['year']}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
